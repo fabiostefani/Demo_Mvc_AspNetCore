d71dc60e61b724c5e8c37df1f7252638ad078fd9
Fabio de Stefani <fabiostefani@gmail.com>
2021-10-04 06:37:45 -0300

    App Completa 1

---
+++ src/fabiostefani.io.Data/Mappings/ProdutoMapping.cs
@@ -0,0 +1,35 @@
+﻿using fabiostefani.io.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace fabiostefani.io.Data.Mappings
+{
+    public class ProdutoMapping : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Nome)
+                .HasMaxLength(200)
+                .IsRequired()
+                .HasColumnName("Nome");
+
+            builder.Property(x => x.Descricao)
+                .HasMaxLength(1000)
+                .IsRequired()
+                .HasColumnName("Descricao");
+
+            builder.Property(x => x.Imagem)
+                .HasMaxLength(100)
+                .IsRequired()
+                .HasColumnName("Imagem");
+
+            builder.Property(x => x.Ativo)
+                .HasColumnName("Ativo")
+                .IsRequired();
+
+            builder.ToTable("Produtos");
+        }
+    }
+}
